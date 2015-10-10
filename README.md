cf57c2ededa8db8fd97a02965d1629dd52ba424e
realdark <me@borislazarov.com>
2015-10-10 17:15:48 +0300

    Final correction

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# wmg-web-api
-A PHP implementation of WMG's (wmg.com) Web API
+# Wmg Web Api
+A PHP implementation of [WMG's](https://tours-api.dsp.wmg.com/) Web API.
+
+### It includes the following
+- Helper methods for all API methods:
+ - Search by artists name.
+ - Search by tour name.
+ - Search by location.
+- PSR-4 autoloading support.
+
+### Requirements
+* PHP 5.4.1 or greater.
+* PHP [cURL extension](http://php.net/manual/en/book.curl.php) (Usually included with PHP).
+
+### Installation
+Add `wmg-web-api` as a dependency to your composer.json:
+```
+"require": {
+    "realdark/wmg-web-api": "1.3.*@dev"
+}
+```
+
+### Examples
+Get all tours by an artist
+```
+$tours = WmgWebApi::searchForArtistEvents("eminem");
+```
+Search for tour by his name
+```
+$tours = WmgWebApi::searchForTour("His Name Is Eminem");
+```
+Get all tours by an country name
+```
+$tours = WmgWebApi::searchForCountryEvents("us");
+```
+
+> All responses are objects
+
+# License
+MIT license. Please see [LICENSE.md](https://github.com/realdark/wmg-web-api/blob/master/LICENSE) for more information.
--- src/WmgWebApi.php
+++ src/WmgWebApi.php
@@ -1,9 +1,11 @@
 <?php
 
-#namespace realdark\WmbWebApi;
+namespace realdark\WmgWebApi;
 
 /**
  * A PHP implementation of WMG's Web API
+ *
+ * @author realdark <me@borislazarov.com> 
  */
 class WmgWebApi
 {
@@ -17,11 +19,19 @@ class WmgWebApi
     
     /**
      * Fetch data
+     * 
+     * @param string $type Artist, tour or country name
+     * @param string $name Search param
+     * @param integer $page Begin fetch from this page
+     * @return object
      */
-    private static function fetchData($type, $name)
+    private static function fetchData($type, $name, $page = null)
     {
+        // Add pages
+        $page = empty($page) ? null : "&page=" . $page;
+        
         // URL
-        $url = "https://tours-api.dsp.wmg.com/shows.json?" . $type . "=" . urlencode($name);
+        $url = "https://tours-api.dsp.wmg.com/shows.json?" . $type . "=" . urlencode($name) . $page;
         
         // Get cURL resource
         $curl = curl_init();
@@ -41,6 +51,12 @@ class WmgWebApi
         return json_decode($resp);
     }
     
+    /**
+     * Search events by artist name
+     * 
+     * @param string $artist Artist name
+     * @return object
+     */
     public static function searchForArtistEvents($artist = null)
     {
         $events = self::fetchData('artist', $artist);
@@ -48,13 +64,38 @@ class WmgWebApi
         return $events;
     }
     
+    /**
+     * Search events by country name
+     * 
+     * @param string $country Country name
+     * @return object
+     */
     public static function searchForCountryEvents($country = null)
     {
         $events = self::fetchData('countryName', $country);
         
+        // We check if there is more than 100 events. If they are mote than 100 fetch next page
+        if ($events->total_entries > 100) {
+            $totalPages = ceil($events->total_entries / 100 );
+            
+            for ($i = 1; $i < $totalPages; $i++) {
+                $tmpEvents = self::fetchData('countryName', $country, $i + 1);
+                
+                foreach ($tmpEvents->shows as $event) {
+                    $events->shows[] = $event;
+                }
+            }
+        }
+        
         return $events;
     }
     
+    /**
+     * Searh for tours by tour name
+     * 
+     * @param string $tour Tour name
+     * @return object
+     */
     public static function searchForTour($tour = null)
     {
         $events = self::fetchData('tourName', $tour);
